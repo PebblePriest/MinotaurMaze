14f6b3b0bd9aeda6a061c55ffc447d2169f7971b
Ross <44909473+CrimsonBurster@users.noreply.github.com>
2021-11-03 12:27:44 -0600

    Started putting together player 2
    Put together a control map, started script and started putting together a player 2 for the game

--- MinotaurMaze/Assets/Scripts/Player/Player2Controller.cs
+++ MinotaurMaze/Assets/Scripts/Player/Player2Controller.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class Player2Controller : MonoBehaviour
 {
-    public static PlayerController instance;
+    public static Player2Controller instance;
 
     private Rigidbody2D theRB;
     //private SpriteRenderer theSR;
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed, jumpForce;
     private float movingSpeed;
     public float attackingMoveSpeed;
-    
+
     private float inputX;
     private bool canDoubleJump;
 
@@ -70,7 +70,7 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = new Vector3(1, 1, 1);
             }
         }
-       
+
         else
         {
             //knock back to left/right
@@ -106,15 +106,15 @@ public class PlayerController : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-            if (context.performed)
-            {
-                inputX = context.ReadValue<Vector2>().x;
-            }
+        if (context.performed)
+        {
+            inputX = context.ReadValue<Vector2>().x;
+        }
 
-            if (context.canceled)
-            {
-                inputX = 0;
-            }
+        if (context.canceled)
+        {
+            inputX = 0;
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -124,7 +124,7 @@ public class PlayerController : MonoBehaviour
             if (context.performed && GroundCheck.instance.isGrounded == true)
             {
                 theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
-                canDoubleJump = true;
+                //canDoubleJump = true;
             }
             else
             {
@@ -144,22 +144,38 @@ public class PlayerController : MonoBehaviour
             isAttacking = true;
 
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-           
-               
-                foreach (Collider2D enemy in hitEnemies)
+
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                if (enemy.gameObject.tag == "Boss")
                 {
-                    if(enemy.gameObject.tag == "Boss")
-                    {
-                        enemy.GetComponent<Enemy>().TakeDamage(comboAttackDamage);
-                    }
-                    if (enemy.gameObject.tag == "Enemy")
-                    {
-                        enemy.GetComponent<CEnemy>().TakeDamage(comboAttackDamage);
-                    }
-                    
+                    enemy.GetComponent<Enemy>().TakeDamage(comboAttackDamage);
+                }
+                if (enemy.gameObject.tag == "Enemy")
+                {
+                    enemy.GetComponent<CEnemy>().TakeDamage(comboAttackDamage);
                 }
 
+            }
 
+
+        }
+    }
+
+    public void Special(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Based on host, perform special ability");
+        }
+    }
+
+    public void Shift(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Force leave host body");
         }
     }
 
@@ -188,5 +204,4 @@ public class PlayerController : MonoBehaviour
         knockBackCounter = .5f;
         theRB.velocity = new Vector2(0, PlayerHealth.instance.knockBackForce);
     }
-
 }
